bc18bd236b901bf75c6b7c024b455e9b73b8743f
Anjalisen66 <anjalisen8203@gmail.com>
2024-07-11 23:14:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Login from "./components/login/Login";
+import Home from "./components/Home/Home";
+import SignupPage from "./components/signup/Signup";
+import Forgot from "./components/forgot/Forgot";
+import Change from "./components/change/Change";
+import About from "./components/aboutus/About";
+import Influencer from "./components/influencer/Influencer";
+import Profile from "./components/Profile/Profile";
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar/>
+      <Routes>
+        
+        <Route path="/login" element={<Login/>}  />
+        <Route path="/" element={<Home/>}  />
+        <Route path="/home" element={<Home/>}  />
+        <Route path="/signup" element={<SignupPage />} />  
+        <Route path="/forgot" element={<Forgot />} />
+        <Route path="/change" element={<Change />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/influencer" element={<Influencer />} />
+        <Route path="/profile" element={<Profile />} />
+
+
+      </Routes>
+      {/* <Footer/> */}
+    </Router>
   );
 }
 
